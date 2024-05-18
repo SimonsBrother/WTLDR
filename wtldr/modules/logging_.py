ac62e618b4e973ca40e743a9d12f77a58c28dc58
SimonsBrother <calebthair@outlook.com>
2024-05-18 18:23:19 +0100

    Fixed logging not showing info logs.

--- wtldr/modules/logging_.py
+++ wtldr/modules/logging_.py
@@ -10,13 +10,8 @@ def create_logger():
     filename = f"wtldr_log_{formatted_current_time}.log"
     file_handler = logging.FileHandler(filename)
     file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-    file_handler.setLevel(logging.INFO)
+    logger.setLevel(logging.INFO)
 
     logger.addHandler(file_handler)
 
     return logger
-
-
-l = create_logger()
-l.info("Test")
-l.exception("TEST")
